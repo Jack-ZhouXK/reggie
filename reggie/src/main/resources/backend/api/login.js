938b91a98eea0b519ff9bb623f52ecae5290a412
zxk <1742210194@qq.com>
2022-06-11 22:24:18 +0800

    reggie初版

--- reggie/src/main/resources/backend/api/login.js
+++ reggie/src/main/resources/backend/api/login.js
@@ -1,16 +1,14 @@
-// 登录
 function loginApi(data) {
-    return $axios({
-        'url': '/employee/login',
-        'method': 'post',
-        data
-    })
+  return $axios({
+    'url': '/employee/login',
+    'method': 'post',
+    data
+  })
 }
 
-// 登出
-function logoutApi() {
-    return $axios({
-        'url': '/employee/logout',
-        'method': 'post',
-    })
+function logoutApi(){
+  return $axios({
+    'url': '/employee/logout',
+    'method': 'post',
+  })
 }
